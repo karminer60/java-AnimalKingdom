a080b462136f91d3ed8a6f9c3724d7e36f57d502
karina-rodriguez <karminer60@gmail.com>
2020-07-08 07:35:11 -0700

    created class Mammals

---
+++ src/classes/AbstractAnimal.java
@@ -0,0 +1,39 @@
+package classes;
+
+abstract class AbstractAnimal
+{
+	protected int yearNamed;
+}
+
+public AbstractAnimal(int yearNamed)
+{
+	maxId++;
+	id = maxId;
+
+	this.yearNamed = yearNamed;
+	this.name = name;
+}
+
+public abstract String move();
+public abstract String breathe();
+public abstract String reproduce();
+
+public int getId() {
+	return id;
+}
+
+public int getYearNamed() {
+	return yearNamed;
+}
+
+public String getName() {
+	return name;
+}
+
+public void setYearNamed(int yearNamed) {
+	this.yearNamed = yearNamed;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
---
+++ src/classes/Mammals.java
@@ -0,0 +1,6 @@
+package classes;
+
+public class Mammals extends AbstractAnimal {
+    public Mammal(int yearNamed, String name) {
+        super(yearNamed, name)
+}
